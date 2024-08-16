bba5a5865b49c80712898b887fe9a06d89fd333b
Anonim17PL <anonim17pl@gmail.com>
2024-08-16 02:53:39 +0200

    Dodaj pliki projektów.

---
+++ KRNL32/dllmain.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2020 Rat431
+// Copyright (c) 2024 Anonim17PL
+#include "main_system.h"
+#define WIN32_LEAN_AND_MEAN
+//#include <cstdio>
+
+int _stdcall DllMain(void* hModule, unsigned long  ul_reason_for_call, void* lpReserved)
+{
+    if (ul_reason_for_call == 1) {
+        KernelF::InitWrapper(hModule);
+        //AllocConsole();
+        //freopen_s(&stream,"CONOUT$", "w",stdout);
+    }
+    return 1;
+}
---
+++ KRNL32_static/KRNL32_static.cpp
@@ -0,0 +1,26 @@
+﻿//No More Sleep base lib
+// Copyright (c) 2024 Anonim17PL
+
+#include "pch.h"
+#include "framework.h"
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+
+//No More Sleep!
+_declspec(dllexport) void __stdcall SleepWr(DWORD dwMilliseconds) {
+	//Sleep(dwMilliseconds);
+	Sleep(0); //Why zero? See: https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-sleep
+	return;
+}
+
+//Additional thread boost for some apps
+_declspec(dllexport) bool __stdcall SetThreadPriorityWr(HANDLE hThread, int nPriority) {
+	return SetThreadPriority(hThread, THREAD_PRIORITY_TIME_CRITICAL);
+}
+_declspec(dllexport) HANDLE __stdcall CreateThreadWr(LPSECURITY_ATTRIBUTES pThreadAttributes, SIZE_T dwStackSize, LPTHREAD_START_ROUTINE lpStartAddress, __drv_aliasesMem LPVOID lpParameter, DWORD dwCreationFlags, LPDWORD lpThreadId) {
+	HANDLE hT = CreateThread(pThreadAttributes, dwStackSize, lpStartAddress, lpParameter, dwCreationFlags, lpThreadId);
+	if (hT != NULL)
+		SetThreadPriority(hT, THREAD_PRIORITY_TIME_CRITICAL);
+	return hT;
+}
